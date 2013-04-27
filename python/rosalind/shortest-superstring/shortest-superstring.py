1c5fcc57d3d2882b782fd0ba06cfe76a1ef34257
Wojciech Ogrodowczyk <wojtek@7tonlnu.pl>
2013-04-27 12:02:09 +0200

    Rosalind: Genome Assembly as Shortest Superstring.

---
+++ python/rosalind/shortest-superstring/shortest-superstring.py
@@ -0,0 +1,31 @@
+import re
+
+f = open("input.txt", "r")
+array = f.readlines()
+
+nodes = {}
+node_name_token = None
+for k in range(len(array)):
+    if re.match('>', array[k]):
+        node_name_token = array[k].strip('\n >')
+        nodes[node_name_token] = ''
+    else:
+        nodes[node_name_token] += array[k].strip()
+
+chromosome_length = len(nodes[node_name_token])
+combination = nodes[node_name_token]
+del nodes[node_name_token]
+
+while nodes != {}:
+    for node, dna in nodes.items():
+        for length in range(chromosome_length, chromosome_length/2, -1):
+            if dna.endswith(combination[:length]):
+                combination = dna + combination[length:]
+                del nodes[node]
+                break
+            if combination.endswith(dna[:length]):
+                combination = combination + dna[length:]
+                del nodes[node]
+                break
+
+print combination
