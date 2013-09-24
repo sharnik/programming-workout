2545976ca025bae59b1f7aa2a09f829d96d4bd78
Wojciech Ogrodowczyk <wojtek@7tonlnu.pl>
2013-09-24 13:15:01 +0200

    More Modules and Functions Elixir exercises.

--- elixir/programming-elixir/ModulesAndFunctions.exs
+++ elixir/programming-elixir/ModulesAndFunctions.exs
@@ -33,3 +33,22 @@ IO.puts "GCD(5, 7): #{AdvMath.gcd(5, 7)}"
 IO.puts "GCD(12, 8): #{AdvMath.gcd(12, 8)}"
 IO.puts "GCD(8, 12): #{AdvMath.gcd(8, 12)}"
 
+defmodule Chop do
+  def guess(answer, answer..answer), do: IO.puts "The answer is: #{answer}."
+  def guess(answer, min..max) do
+    IO.puts "Is it #{max - div(max - min, 2)}?"
+    guess(answer, adjust_range(answer, min..max))
+  end
+
+  def adjust_range(target, min..max) when (max - div(max - min, 2)) > target do
+    min..(max - div(max - min, 2))
+  end
+  def adjust_range(target, min..max) when (max - div(max - min, 2)) < target do
+    (max - div(max - min, 2))..max
+  end
+  def adjust_range(target, min..max) when (max - div(max - min, 2)) == target do
+    target..target
+  end
+end
+
+Chop.guess(273, 1..1000)
