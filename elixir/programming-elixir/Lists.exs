f2fa8aa8e80106e48cee60a5000f500afec888fd
Wojciech Ogrodowczyk <wojtek@7tonlnu.pl>
2013-09-26 08:52:35 +0200

    More List exercises.

--- elixir/programming-elixir/Lists.exs
+++ elixir/programming-elixir/Lists.exs
@@ -29,3 +29,52 @@ IO.puts "Max [1, 2, 3]: #{MyList.max([2, 3, 1])}"
 
 IO.puts MyList.caesar('xhejn', 13)
 IO.puts inspect(MyList.span(1,3))
+
+defmodule MyEnum do
+  def all?([], _func), do: true
+  def all?([head | tail], func) do
+    if func.(head) do
+      all?(tail, func)
+    else
+      false
+    end
+  end
+
+  def each([elem], func), do: func.(elem)
+  def each([head | tail], func) do
+    func.(head)
+    each(tail, func)
+  end
+
+  def filter([], _func), do: []
+  def filter([head | tail], func) do
+    if func.(head) do
+      [head | filter(tail, func)]
+    else
+      filter(tail, func)
+    end
+  end
+
+  def split(list, count), do: _split(list, [], count)
+  defp _split(first_list, second_list, 0), do: { first_list, second_list }
+  defp _split([head | tail], list, count), do: _split(tail, [head | list], count - 1)
+
+  def take(list, 0), do: []
+  def take([head | tail], count), do: [head | take(tail, count - 1)]
+
+  def flatten([]), do: []
+  def flatten([head | tail]) when is_list(head) do
+    flatten(head) ++ flatten(tail)
+  end
+  def flatten([head | tail]) do
+    [head | flatten(tail)]
+  end
+
+end
+
+IO.puts inspect(MyEnum.all?([1, 2, 3], &(&1 > 1)))
+MyEnum.each(['bla1', 'bla2', 'bla3'], &(IO.puts(&1)))
+IO.puts inspect(MyEnum.filter([2, 1, 3], &(&1 > 1)))
+IO.puts inspect(MyEnum.split([2, 1, 3], 2))
+IO.puts inspect(MyEnum.take([2, 1, 3], 2))
+IO.puts inspect(MyEnum.flatten([ 1, [ 2, 3, [4] ], 5, [[[6]]]]))
