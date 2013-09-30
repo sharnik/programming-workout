d2bba7bde64b4e0cbab3b92505fed9065ba6bbe1
Wojciech Ogrodowczyk <wojtek@7tonlnu.pl>
2013-09-30 15:14:45 +0200

    Rosalind: Enumerating k-mers Lexicographically.

---
+++ elixir/rosalind/enumerating-kmers-lexicographically.exs
@@ -0,0 +1,25 @@
+defmodule Rosalind do
+
+  def permutations(_, 0), do: [[]]
+  def permutations(list, length) do
+    lc head inlist list, tail inlist permutations(list, length - 1) do
+      [head | tail]
+    end
+  end
+
+  def compare([a | tail_a], [b | tail_b], alphabet) do
+    if a == b do
+      compare(tail_a, tail_b, alphabet)
+    else
+      Enum.find_index(alphabet, &1 == a) < Enum.find_index(alphabet, &1 == b)
+    end
+  end
+
+end
+
+alphabet = IO.read(:line) |> String.split
+{length, _} = IO.read(:line) |> String.to_integer
+
+Rosalind.permutations(alphabet, length)
+  |> Enum.sort(&(Rosalind.compare(&1, &2, alphabet)))
+  |> Enum.each(IO.puts(&1))
