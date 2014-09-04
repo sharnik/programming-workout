4b617ccf300da07ebe1def1f35f4e8c5b5a14362
Wojciech Ogrodowczyk <wojtek@7tonlnu.pl>
2014-09-04 17:45:07 +0200

    Clojure: Seven Languages, day 2

--- clojure/seven-languages/src/seven_languages/core.clj
+++ clojure/seven-languages/src/seven_languages/core.clj
@@ -1,10 +1,5 @@
 (ns seven-languages.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
-
 (defn big
   [st n]
   (> (count st) n))
@@ -12,3 +7,26 @@
 (defn collection-type
   [col]
   ({ clojure.lang.PersistentList :list, clojure.lang.PersistentArrayMap :map, clojure.lang.PersistentVector :vector} (type col)))
+
+
+(defmacro unless [test body otherwise]
+  (list 'if (list 'not test) body otherwise))
+
+
+(defprotocol Compass
+  (direction [c])
+  (left [c])
+  (right [c]))
+
+(def directions [:north :east :south :west])
+
+(defn turn [base amount]
+  (rem (+ base amount) (count directions)))
+
+(defrecord SimpleCompass [bearing]
+  Compass
+  (direction [_] (directions bearing))
+  (left [_] (SimpleCompass. (turn bearing 3)))
+  (right [_] (SimpleCompass. (turn bearing 1)))
+  Object
+  (toString [this] (str "[" (direction this) "]")))
