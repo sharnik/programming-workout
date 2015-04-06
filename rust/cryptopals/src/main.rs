1a77fdd18f4545685a50bb3218de3f4a5748f5b4
Wojciech Ogrodowczyk <wojciech@haikuco.de>
2015-04-06 09:06:09 +0200

    Rust cryptopals: setting up the project

---
+++ rust/cryptopals/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    println!("Hello World.\n");
+}
+
+fn hex_to_base64(hex: &str) -> &str {
+    "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+}
+
+#[test]
+fn test_hex_to_base64() {
+    assert!(hex_to_base64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d") == "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t")
+}
