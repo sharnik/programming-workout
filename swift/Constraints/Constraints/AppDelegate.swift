32b7ff28eaf8adab686520c345668139f20b9cff
Wojciech Ogrodowczyk <wojciech@haikuco.de>
2015-04-08 15:01:45 +0200

    Swift: Practice app for Constraints

---
+++ swift/Constraints/Constraints/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Constraints
+//
+//  Created by Wojciech Ogrodowczyk on 08/04/15.
+//  Copyright (c) 2015 Wojciech Ogrodowczyk. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
