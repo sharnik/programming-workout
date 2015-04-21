1fd756d6c2130d95f4bad71e148fafe490753c4d
Wojciech Ogrodowczyk <wojciech@haikuco.de>
2015-04-21 07:27:29 +0200

    [Rust] Scanning letter frequency

--- rust/cryptopals/src/set_1.rs
+++ rust/cryptopals/src/set_1.rs
@@ -8,6 +8,23 @@ pub fn xor_against_single_char(string: Vec<u8>, xor_char: u8) -> Vec<u8> {
     result
 }
 
+fn scan_letter_frequency(string: Vec<u8>) -> [u16; 26] {
+    let mut result : [u16; 26] = [0; 26];
+    for k in 0..25 { result[k] = 0 }
+    for letter in string {
+        println!("{}", letter);
+        if letter >= 65 && letter <= 90 {
+            let idx : u8 = letter - 65;
+            result[idx as usize] += 1;
+        } else if letter >= 97 && letter <= 122 {
+            let idx : u8 = letter - 97;
+            result[idx as usize] += 1;
+        };
+    }
+
+    result
+}
+
 #[test]
 fn test_single_char_xor() {
     let string = helpers::hex_to_byte_string("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736".
@@ -19,4 +36,11 @@ fn test_single_char_xor() {
     };
 
     assert!(result == "Cooking MC\'s like a pound of bacon".to_string());
+}
+
+#[test]
+fn test_scan_letter_frequency() {
+    assert!(scan_letter_frequency("Foo bar".to_string().into_bytes()) ==
+        [1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0]
+    )
 }
