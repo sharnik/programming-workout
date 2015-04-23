19a30e0d0358e8407dc1db63bf646dd4dde56a94
Wojciech Ogrodowczyk <wojciech@haikuco.de>
2015-04-23 17:06:20 +0200

    [Rust] Finally properly score strings

--- rust/cryptopals/src/set_1.rs
+++ rust/cryptopals/src/set_1.rs
@@ -8,16 +8,16 @@ pub fn xor_against_single_char(string: Vec<u8>, xor_char: u8) -> Vec<u8> {
     result
 }
 
+// Scans the string and counts letter occurences
 fn scan_letter_frequency(string: Vec<u8>) -> [u16; 26] {
     let mut result : [u16; 26] = [0; 26];
     for letter in string {
-        println!("{}", letter);
         if letter >= 65 && letter <= 90 {
             let idx : u8 = letter - 65;
             result[idx as usize] += 1;
         } else if letter >= 97 && letter <= 122 {
             let idx : u8 = letter - 97;
-            result[idx as usize] += 1;
+            result[idx as usize] = result[idx as usize ] + 1;
         };
     }
 
@@ -25,17 +25,19 @@ fn scan_letter_frequency(string: Vec<u8>) -> [u16; 26] {
 }
 
 // Scores the string if it's close to natural frequency. The lower, the beter.
-fn score_string(occurence: [u16; 26], length: u16) -> u16 {
+fn score_string(occurence: [u16; 26], length: u16) -> f32 {
     // This is expected (usual) frequency of particular letters in English phrases
-    let expected_frequency = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-    let mut scores = [0; 26];
-    for idx in 0..25 {
-        let letter_score = occurence[idx] * 100 / length;
-        scores[idx] = expected_frequency[idx] - letter_score // TODO: absolute value of score
+    let expected_frequency = [8.167, 1.492, 2.782, 4.253, 12.702, 2.228, 2.015, 6.094, 6.966, 0.153,
+        0.772, 4.025, 2.406, 6.749, 7.507, 1.929, 0.095, 5.987, 6.327, 9.056, 2.758, 0.978, 2.360,
+        0.150, 1.974, 0.074];
+    let mut scores = [0.0; 26];
+    for idx in 0..26 {
+        let letter_score : f32 = (occurence[idx] as f32) * 100.0 / (length as f32);
+        scores[idx] = (expected_frequency[idx] - letter_score).abs();
     };
 
-    let mut final_score : u16= 0;
-    for score in scores.iter() { final_score += *score };
+    let mut final_score : f32 = 0.0;
+    for score in scores.iter() { final_score = final_score + *score };
 
     final_score
 }
@@ -44,8 +46,7 @@ fn score_string(occurence: [u16; 26], length: u16) -> u16 {
 fn test_score_string() {
     // Let's test "Foo bar"
     let input = [1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0];
-    println!("{:?}", score_string(input, 7));
-
+    assert!(score_string(input, 7) == 134.951294);
 }
 
 
