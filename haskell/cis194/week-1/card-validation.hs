52911d315dafab3c05899d3ad79469a9ef540ea2
Wojciech Ogrodowczyk <wojciech@haikuco.de>
2015-05-18 11:22:15 +0200

    [Haskell] CIS194, week 1, exercise 5
    Tower of Hanoi.

--- haskell/cis194/week-1/card-validation.hs
+++ haskell/cis194/week-1/card-validation.hs
@@ -1,6 +1,7 @@
--- Exercise 1-4 for CIS194 course, week 1
+-- CIS194 course, week 1
 -- https://www.cis.upenn.edu/~cis194/lectures.html
 
+-- Exercises 1-4
 toDigits :: Integer -> [Integer]
 toDigits int
   | int <= 0 = []
@@ -26,7 +27,19 @@ sumDigits (x:xs)
 
 validate :: Integer -> Bool
 validate number =
- let sum = sumDigits (doubleEveryOther (toDigits number)) in
- (mod sum 10) == 0
+  let sum = sumDigits (doubleEveryOther (toDigits number)) in
+  (mod sum 10) == 0
 
-main = print (validate 4012888888881882)
+
+-- Exercise 5
+
+type Peg = String
+type Move = (Peg, Peg)
+
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi 1 peg_a peg_b peg_c = [(peg_a, peg_b)]
+hanoi x peg_a peg_b peg_c =
+  (hanoi (x-1) peg_a peg_c peg_b) ++ [(peg_a, peg_b)] ++ (hanoi (x-1) peg_c peg_b peg_a)
+
+main = print (hanoi 3 "a" "b" "c")
+
