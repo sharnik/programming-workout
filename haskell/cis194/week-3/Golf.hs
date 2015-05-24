c231ae02ee30d4f878e6eccd998aebd9a5f540f5
Wojciech Ogrodowczyk <wojciech@haikuco.de>
2015-05-24 14:27:01 +0200

    [Haskell] CIS194, week 3, exercise 3
    Histogram.

--- haskell/cis194/week-3/Golf.hs
+++ haskell/cis194/week-3/Golf.hs
@@ -25,3 +25,31 @@ localMaxima (left:middle:right:els)
   | otherwise = (localMaxima (middle:right:els))
 localMaxima _ = []
 
+-- Exercise 3
+
+incrementResult :: [Int] -> Int -> [Int]
+incrementResult xs index =
+  let current_value = xs !! index in
+  (take index xs) ++ [current_value + 1] ++ (drop (index + 1) xs)
+
+countOccurences :: [Int] -> [Int]
+countOccurences list =
+  _countOccurences list (replicate 10 0)
+
+_countOccurences :: [Int] -> [Int] -> [Int]
+_countOccurences [] result = result
+_countOccurences (x:xs) result =
+  _countOccurences xs (incrementResult result x)
+
+prepareLine :: Int -> [String] -> String
+prepareLine index list = (map (\str -> str !! index) list)
+
+histogram :: [Int] -> String
+histogram list =
+  let occurs = countOccurences list in
+  let max = maximum occurs in
+  let star_string = map (\x -> (replicate (max - x) ' ') ++ (replicate x '*')) occurs in
+  let lines = map (\x -> (prepareLine x star_string)) [0, 1..max-1] in
+  let underscore = "==========\n0123456789\n" in
+  (unlines lines) ++ underscore
+
