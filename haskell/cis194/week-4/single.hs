3852d3c7c10710f1e9a20b74b94e73371eb5a20f
Wojciech Ogrodowczyk <wojciech@haikuco.de>
2015-05-28 18:00:51 +0200

    [Haskell] CIS194, week 4, exercise 3
    Sieve of Sundaram

--- haskell/cis194/week-4/single.hs
+++ haskell/cis194/week-4/single.hs
@@ -8,4 +8,14 @@ fun1 =  foldr (*) 1 . map (\x -> x - 2) . filter (\x -> (mod x 2) == 0)
 -- TODO: Solve :sad_panda:
 -- fun2 :: Integer -> Integer
 
-main = print (fun1 [3,4,5,6,7])
+-- Exercise 3, Sieve of Sundaram
+
+sieveSundaram :: Integer -> [Integer]
+sieveSundaram =
+  map (\x -> 2 * x + 1)
+  . (\n ->
+    let removables = [i + j + 2*i*j | i<-[1..n], j<-[i..n], i + j + 2*i*j <= n]
+    in filter (\x -> (not (x `elem` removables))) [1..n]
+  )
+
+main = print (sieveSundaram 15)
