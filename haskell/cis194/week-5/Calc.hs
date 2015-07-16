26b09f26b01b8083bc1bfb62d94e8536b306a063
Wojciech Ogrodowczyk <wojciech@haikuco.de>
2015-07-16 17:55:30 +0200

    [Haskell] CIS194, week 5, exercise 4

--- haskell/cis194/week-5/Calc.hs
+++ haskell/cis194/week-5/Calc.hs
@@ -20,6 +20,32 @@ module Calc where
         add x y = (Add x y)
         mul x y = (Mul x y)
 
+    instance Expr Integer where
+        lit x = x
+        add x y = x + y
+        mul x y = x * y
+
+    instance Expr Bool where
+        lit x
+            | x > 0 = True
+            | otherwise = False
+        add x y = x || y
+        mul x y = x && y
+
+    newtype MinMax = MinMax Integer deriving (Eq, Show)
+
+    instance Expr MinMax where
+        lit x = MinMax x
+        add (MinMax x) (MinMax y) = MinMax (max x y)
+        mul (MinMax x) (MinMax y) = MinMax (min x y)
+
+    newtype Mod7 = Mod7 Integer deriving (Eq, Show)
+
+    instance Expr Mod7 where
+        lit x = Mod7 x
+        add (Mod7 x) (Mod7 y)  = Mod7 ((x + y) `mod` 7)
+        mul (Mod7 x) (Mod7 y) = Mod7 ((x * y) `mod` 7)
+
     eval :: ExprT -> Integer
     eval x = evaluate x
 
@@ -29,4 +55,12 @@ module Calc where
         in case e of
             Nothing -> Nothing
             Just x -> Just (eval x)
+
+    testExp :: Expr a => Maybe a
+    testExp = parseExp lit add mul "(3 * -4) + 5"
+
+    testInteger = testExp :: Maybe Integer
+    testBool = testExp :: Maybe Bool
+    testMinMax = testExp :: Maybe MinMax
+    testMod7 = testExp :: Maybe Mod7
 
--- haskell/cis194/week-5/Test.hs
+++ haskell/cis194/week-5/Test.hs
@@ -5,4 +5,7 @@ import ExprT
 -- main = print( eval( Mul (Add (Lit 2) (Lit 3)) (Lit 4)))
 
 -- Exercise 2
-main = print( evalStr "2+3*4" )
+-- main = print( evalStr "2+3*4" )
+
+-- Exercise 4
+main = print testMod7
