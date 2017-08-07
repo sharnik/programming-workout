5bb8cf42eac3dc88f33c4582c7d58f13c9974b78
Wojciech Ogrodowczyk <wojciech@brainsandbeards.com>
2017-08-07 18:03:42 +0200

    Cryptopals in Swift: set 1, exercise 2

--- swift/cryptopals/set-1.swift
+++ swift/cryptopals/set-1.swift
@@ -1,8 +1,8 @@
 import Foundation
 
-// Exercise #1
 extension Data {
-  
+
+  // Exercise #1
   public init?(hexString: String) {
     var characters = hexString.characters
     self.init(capacity: characters.count / 2)
@@ -19,16 +19,38 @@ extension Data {
     }
   }
 
+  // Exercise #1
   func base64String() -> String {
     return self.base64EncodedString()
+  }
+
+  func hexString() -> String {
+    return map{ String(format: "%02hhx", $0) }.joined()
   }
 
   func printableString() -> String {
     return String(data: self, encoding: String.Encoding.utf8)!
   }
+
+  // Exercise #2
+  func xor(otherData: Data) -> Data {
+    var result = Data(capacity: self.count)
+    for (index, element) in self.enumerated() {
+      let xoredValue = element ^ (otherData[index] as UInt8)
+      result.append(xoredValue)
+    }
+    return result
+  }
 }
 
-let bytes = Data(hexString: "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")!
-print(bytes.base64String())
-print(bytes.printableString())
+// Exercise #1
+// let bytes = Data(hexString: "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")!
+// print(bytes.base64String())
+// print(bytes.printableString())
 
+// Exercise #2
+// let hexOne = Data(hexString: "1c0111001f010100061a024b53535009181c")!
+// let hexTwo = Data(hexString: "686974207468652062756c6c277320657965")!
+// let resultHex = hexOne.xor(otherData: hexTwo)
+// print(resultHex.hexString())
+// print(resultHex.printableString())
